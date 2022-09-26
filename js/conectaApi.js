50ffdd2a4d351f03646d88f93f4014d62335e83b
Adriano <adriano_alves_2008@hotmail.com>
2022-09-26 11:50:54 -0300

    requisição post

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,6 +5,28 @@ async function listaVideos() {
   return conexaoConvertida
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+  // Por padrão vem GET, então alteramos para POST
+  const conexao = await fetch('http://localhost:3000/videos', {
+    method: 'POST',
+    headers: {
+      // Serve para especificar que tipo de arquivo está sendo enviado ou recebido
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      url: url,
+      imagem: imagem
+    })
+  })
+
+  const conexaoConvertida = await conexao.json()
+
+  return conexaoConvertida
+}
+
 export const conectaApi = {
-  listaVideos
+  listaVideos,
+  criaVideo
 }
