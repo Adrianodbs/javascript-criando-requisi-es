7ab75737d74053b485d63c22ce5f3e998b920ab3
Adriano <adriano_alves_2008@hotmail.com>
2022-09-26 15:29:02 -0300

    busca dinamica

---
+++ js/buscarVideo.js
@@ -0,0 +1,10 @@
+import { conectaApi } from './conectaAPI'
+
+async function buscarVideo(evento) {
+  evento.preventDefault()
+  const dadosDePesquisa = document.querySelector('[data-pesquisa]').value
+  const busca = await conectaApi.buscaVideo(dadosDePesquisa)
+}
+
+const botaoDePesquisa = document.querySelector('[data-botao-pesquisa]')
+botaoDePesquisa.addEventListener('click', evento => buscarVideo(evento))
